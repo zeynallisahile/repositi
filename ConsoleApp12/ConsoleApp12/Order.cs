3f98e29e8e8ffb9632497ccfb3cc8720635a7dce
zeynallisahile <tu7dfbyuu@code.edu.az>
2022-04-06 11:50:17 +0400

    Task Completed

---
+++ ConsoleApp12/ConsoleApp12/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    class Order
+    {
+        private static int _no;
+        public int No { get;}
+        public int ProductCount { get; set; }
+        public double TotalAmount { get; set; }
+
+        private static DateTime _createdAt;
+        public DateTime CreatedAt { get;}
+        public Order()
+        {
+            _no++;
+            No = _no;
+            _createdAt = DateTime.UtcNow.Date;
+            CreatedAt = _createdAt;
+        }
+    }
+}
---
+++ ConsoleApp12/ConsoleApp12/Shop.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    class Shop
+    {
+        private List<Order> _orders;
+        public Shop()
+        {
+            _orders = new List<Order>();
+        }     
+        public void AddOrder(Order order)
+        {
+            _orders.Add(order);
+        }
+
+        public void GetOrdersAvg()
+        {
+
+        }
+
+        public double TotalAlmount(double sum)
+        {
+            sum = 0;
+            foreach (var item in _orders)
+            {
+                sum = sum + item.TotalAmount;
+            }
+            sum /=_orders.Count;
+            return sum;
+        }
+        public void GetOrdersAvg(DateTime dateTime)
+        {
+            List<Order> filterPrice = new List<Order>();           
+             
+        }
+
+        public void RemoveOrderByNo(int no)
+        {            
+            _orders.Remove(_orders.Find(x => x.No == no));
+        }
+
+        public List<Order>FilterOrderByPrice(int min,int max)
+        {
+            List<Order> filterOrderByPrice = new List<Order>();
+            var result = _orders.Find(x => x.TotalAmount >= min && x.TotalAmount <= max);
+            return filterOrderByPrice;
+        }
+    }
+}
